4eec9ae41e0f59ab3f07b76cb4ac1eb3691fc117
nikijey031-oss <nikijey031@gmail.com>
2026-08-18 20:45:26 +0530

    Update priority.c

--- Experiment-04/priority.c
+++ Experiment-04/priority.c
@@ -1,2 +1,56 @@
 #include <stdio.h>
-int main(){int n,i,j,t,bt[20],pr[20],wt[20],tat[20];float aw=0,at=0;printf("Enter Number of Processes: ");scanf("%d",&n);for(i=0;i<n;i++){printf("Enter Burst Time for P%d: ",i+1);scanf("%d",&bt[i]);printf("Enter Priority for P%d: ",i+1);scanf("%d",&pr[i]);}for(i=0;i<n-1;i++)for(j=i+1;j<n;j++)if(pr[i]>pr[j]){t=pr[i];pr[i]=pr[j];pr[j]=t;t=bt[i];bt[i]=bt[j];bt[j]=t;}wt[0]=0;for(i=1;i<n;i++)wt[i]=wt[i-1]+bt[i-1];printf("\nProcess\tPriority\tBT\tWT\tTAT\n");for(i=0;i<n;i++){tat[i]=wt[i]+bt[i];aw+=wt[i];at+=tat[i];printf("P%d\t%d\t\t%d\t%d\t%d\n",i+1,pr[i],bt[i],wt[i],tat[i]);}printf("\nAverage Waiting Time = %.2f\nAverage Turnaround Time = %.2f\n",aw/n,at/n);return 0;}
+
+int main()
+{
+    int n, i, j, temp;
+    int bt[20], pr[20], wt[20], tat[20];
+    float awt = 0, atat = 0;
+
+    printf("Enter Number of Processes: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter Burst Time for P%d: ", i + 1);
+        scanf("%d", &bt[i]);
+
+        printf("Enter Priority for P%d: ", i + 1);
+        scanf("%d", &pr[i]);
+    }
+
+    for (i = 0; i < n - 1; i++)
+        for (j = i + 1; j < n; j++)
+            if (pr[i] > pr[j])
+            {
+                temp = pr[i];
+                pr[i] = pr[j];
+                pr[j] = temp;
+
+                temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
+            }
+
+    wt[0] = 0;
+
+    for (i = 1; i < n; i++)
+        wt[i] = wt[i - 1] + bt[i - 1];
+
+    printf("\nProcess\tPriority\tBT\tWT\tTAT\n");
+
+    for (i = 0; i < n; i++)
+    {
+        tat[i] = wt[i] + bt[i];
+
+        awt += wt[i];
+        atat += tat[i];
+
+        printf("P%d\t%d\t\t%d\t%d\t%d\n",
+               i + 1, pr[i], bt[i], wt[i], tat[i]);
+    }
+
+    printf("\nAverage Waiting Time = %.2f", awt / n);
+    printf("\nAverage Turnaround Time = %.2f\n", atat / n);
+
+    return 0;
+}

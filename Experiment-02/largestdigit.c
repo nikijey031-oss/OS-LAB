93fcc763f3816f60fd0e18949615db3445563dd3
nikijey031-oss <nikijey031@gmail.com>
2026-08-18 20:50:10 +0530

    Update largestdigit.c

--- Experiment-02/largestdigit.c
+++ Experiment-02/largestdigit.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
-int main(){int n,d,m=0;printf("ENTER THE NUMBER: ");scanf("%d",&n);while(n>0){d=n%10;if(d>m)m=d;n/=10;}printf("THE LARGEST DIGIT OF THE NUMBER: %d\n",m);return 0;}
+
+int main()
+{
+    int n, digit, max = 0;
+
+    printf("ENTER THE NUMBER: ");
+    scanf("%d", &n);
+
+    while (n > 0)
+    {
+        digit = n % 10;
+
+        if (digit > max)
+            max = digit;
+
+        n /= 10;
+    }
+
+    printf("THE LARGEST DIGIT OF THE NUMBER: %d\n", max);
+
+    return 0;
+}

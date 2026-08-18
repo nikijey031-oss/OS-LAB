432834bada54f3475eee0a0810ab7b2fa7ecbae7
nikijey031-oss <nikijey031@gmail.com>
2026-08-18 20:44:21 +0530

    Update roundrobin.c

--- Experiment-04/roundrobin.c
+++ Experiment-04/roundrobin.c
@@ -1,2 +1,64 @@
 #include <stdio.h>
-int main(){int n,q,i,time=0,done,bt[20],r[20],wt[20]={0},tat[20];float aw=0,at=0;printf("Enter Number of Processes: ");scanf("%d",&n);for(i=0;i<n;i++){printf("Enter Burst Time for P%d: ",i+1);scanf("%d",&bt[i]);r[i]=bt[i];}printf("Enter Time Quantum: ");scanf("%d",&q);do{done=1;for(i=0;i<n;i++)if(r[i]>0){done=0;if(r[i]>q){time+=q;r[i]-=q;}else{time+=r[i];wt[i]=time-bt[i];r[i]=0;}}}while(!done);printf("\nProcess\tBT\tWT\tTAT\n");for(i=0;i<n;i++){tat[i]=bt[i]+wt[i];aw+=wt[i];at+=tat[i];printf("P%d\t%d\t%d\t%d\n",i+1,bt[i],wt[i],tat[i]);}printf("\nAverage Waiting Time = %.2f\nAverage Turnaround Time = %.2f\n",aw/n,at/n);return 0;}
+
+int main()
+{
+    int n, tq, i, time = 0, done;
+    int bt[20], rem[20], wt[20] = {0}, tat[20];
+    float awt = 0, atat = 0;
+
+    printf("Enter Number of Processes: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter Burst Time for P%d: ", i + 1);
+        scanf("%d", &bt[i]);
+        rem[i] = bt[i];
+    }
+
+    printf("Enter Time Quantum: ");
+    scanf("%d", &tq);
+
+    do
+    {
+        done = 1;
+
+        for (i = 0; i < n; i++)
+        {
+            if (rem[i] > 0)
+            {
+                done = 0;
+
+                if (rem[i] > tq)
+                {
+                    time += tq;
+                    rem[i] -= tq;
+                }
+                else
+                {
+                    time += rem[i];
+                    wt[i] = time - bt[i];
+                    rem[i] = 0;
+                }
+            }
+        }
+    } while (!done);
+
+    printf("\nProcess\tBT\tWT\tTAT\n");
+
+    for (i = 0; i < n; i++)
+    {
+        tat[i] = bt[i] + wt[i];
+
+        awt += wt[i];
+        atat += tat[i];
+
+        printf("P%d\t%d\t%d\t%d\n",
+               i + 1, bt[i], wt[i], tat[i]);
+    }
+
+    printf("\nAverage Waiting Time = %.2f", awt / n);
+    printf("\nAverage Turnaround Time = %.2f\n", atat / n);
+
+    return 0;
+}

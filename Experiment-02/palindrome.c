87f23d85bb6fccdf390ef5e8ad9fd1702006a8d5
nikijey031-oss <nikijey031@gmail.com>
2026-08-18 20:51:03 +0530

    Update palindrome.c

--- Experiment-02/palindrome.c
+++ Experiment-02/palindrome.c
@@ -1,3 +1,33 @@
 #include <stdio.h>
 #include <string.h>
-int main(){char s[100];int i,j,ok=1;printf("ENTER THE STRING TO CHECK PALINDROME: ");scanf("%99s",s);j=(int)strlen(s)-1;for(i=0;i<j;i++,j--)if(s[i]!=s[j]){ok=0;break;}printf("%s %s a palindrome\n",s,ok?"is":"is not");return 0;}
+
+int main()
+{
+    char str[100];
+    int i, j, flag = 1;
+
+    printf("ENTER THE STRING TO CHECK PALINDROME: ");
+    scanf("%s", str);
+
+    i = 0;
+    j = strlen(str) - 1;
+
+    while (i < j)
+    {
+        if (str[i] != str[j])
+        {
+            flag = 0;
+            break;
+        }
+
+        i++;
+        j--;
+    }
+
+    if (flag)
+        printf("%s is a palindrome\n", str);
+    else
+        printf("%s is not a palindrome\n", str);
+
+    return 0;
+}

a959816959de8e44d815ee7ee4a6bb832c53c05c
nikijey031-oss <nikijey031@gmail.com>
2026-08-18 20:53:33 +0530

    Update fork_getpid_wait_exit.c

--- Experiment-03/fork_getpid_wait_exit.c
+++ Experiment-03/fork_getpid_wait_exit.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 #include <windows.h>
-int main(){printf("Child Process Created\nProcess ID: %lu\nChild Process Executing...\n",GetCurrentProcessId());Sleep(2000);printf("Child Process Exiting\nParent Process Waiting...\n");Sleep(1000);printf("Parent Process Resumes Execution\n");return 0;}
+
+int main()
+{
+    printf("Child Process Created\n");
+    printf("Process ID: %lu\n", GetCurrentProcessId());
+
+    printf("Child Process Executing...\n");
+
+    Sleep(2000);
+
+    printf("Child Process Exiting\n");
+
+    printf("Parent Process Waiting...\n");
+
+    Sleep(1000);
+
+    printf("Parent Process Resumes Execution\n");
+
+    return 0;
+}

db2cb86d6d21651d2de1746ae8c0beaadf9f1b76
nikijey031-oss <nikijey031@gmail.com>
2026-08-18 20:52:59 +0530

    Update wait.c

--- Experiment-03/wait.c
+++ Experiment-03/wait.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 #include <windows.h>
-int main(){printf("Child Process Running\n");Sleep(5000);printf("Child Process Completed\nParent Resumes Execution\n");return 0;}
+
+int main()
+{
+    printf("Child Process Running\n");
+
+    Sleep(5000);
+
+    printf("Child Process Completed\n");
+
+    printf("Parent Resumes Execution\n");
+
+    return 0;
+}

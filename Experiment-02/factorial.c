920b9ef4535672f36c8f45bce3e9af49c5acb3ac
nikijey031-oss <nikijey031@gmail.com>
2026-08-18 20:47:51 +0530

    Update factorial.c

--- Experiment-02/factorial.c
+++ Experiment-02/factorial.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
-int main(){int n,i; long long f=1; printf("ENTER THE NUMBER: "); scanf("%d",&n); for(i=1;i<=n;i++)f*=i; printf("FACTORIAL OF THE GIVEN NUMBER IS %lld\n",f); return 0;}
+
+int main()
+{
+    int n, i;
+    long long fact = 1;
+
+    printf("ENTER THE NUMBER: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+        fact *= i;
+
+    printf("FACTORIAL OF THE GIVEN NUMBER IS %lld\n", fact);
+
+    return 0;
+}

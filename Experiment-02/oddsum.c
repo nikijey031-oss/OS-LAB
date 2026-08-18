78d0523805ce3e8406b32700e64e00072eee769e
nikijey031-oss <nikijey031@gmail.com>
2026-08-18 20:48:29 +0530

    Update oddsum.c

--- Experiment-02/oddsum.c
+++ Experiment-02/oddsum.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
-int main(){int n,i,s=0; printf("ENTER THE LIMIT: "); scanf("%d",&n); for(i=1;i<=n;i+=2)s+=i; printf("SUM = %d\n",s); return 0;}
+
+int main()
+{
+    int n, i, sum = 0;
+
+    printf("ENTER THE LIMIT: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i += 2)
+        sum += i;
+
+    printf("SUM = %d\n", sum);
+
+    return 0;
+}

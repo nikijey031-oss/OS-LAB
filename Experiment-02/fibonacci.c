fb21ecfddb3d87bc06f7f3bee3f76ef8236544f8
nikijey031-oss <nikijey031@gmail.com>
2026-08-18 20:49:04 +0530

    Update fibonacci.c

--- Experiment-02/fibonacci.c
+++ Experiment-02/fibonacci.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
-int main(){int n,i,a=0,b=1,c; printf("ENTER THE LIMIT: "); scanf("%d",&n); printf("FIBONACCI SERIES:\n"); for(i=1;i<=n;i++){c=a+b;printf("%d ",c);a=b;b=c;} printf("\n"); return 0;}
+
+int main()
+{
+    int n, i, a = 0, b = 1, c;
+
+    printf("ENTER THE LIMIT: ");
+    scanf("%d", &n);
+
+    printf("FIBONACCI SERIES:\n");
+
+    for (i = 1; i <= n; i++)
+    {
+        c = a + b;
+        printf("%d ", c);
+        a = b;
+        b = c;
+    }
+
+    printf("\n");
+
+    return 0;
+}
